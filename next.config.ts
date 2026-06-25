a406461372905f96964b81bd9500cf1620481067
Devansh Gupta <shopykarttt@gmail.com>
2026-06-25 07:27:59 +0000

    Arey yrr mai baar baar investigation karunga kya nhi karo disable dev to

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,10 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  devIndicators: {
+    appIsrStatus: false,
+    buildActivity: false,
+  },
   images: {
     remotePatterns: [
       {
